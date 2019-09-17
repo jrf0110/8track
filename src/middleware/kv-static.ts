77d8cee863e006e026757f04596a9e157c9032e8
Victoria <bernard.victoria22@gmail.com>
2019-09-17 15:42:16 -0500

    Unfinished POC for setting options passed to kvStatic

--- src/middleware/kv-static.ts
+++ src/middleware/kv-static.ts
@@ -1,16 +1,34 @@
 import { Middleware } from '../Router'
 import { KVNamespace } from '@cloudflare/workers-types'
+const cache = { put: (path: string, num: number) => {} }
 
-interface KVStaticOptions {
-  kv: KVNamespace
-  maxAge?: number
+type CacheOptions = {
+  browserTTL: number
+  edgeTTL: number
+  bypassCache: boolean
 }
-
+type SiteInit = {
+  /* handle requests that don't exist on the bucket */
+  notFoundHandler: (req: Request) => Response | Promise<Response>
+  /* configure how the incoming request's path is found in the bucket */
+  pathToKeyModifier: (path: string) => string
+  /* control the cache for all the site's content or on per request bias */
+  cacheControl: CacheOptions | ((req: Request) => CacheOptions)
+}
+const defaultSiteInit: SiteInit = {
+  notFoundHandler: () => new Response('Not Found', { status: 404 }),
+  pathToKeyModifier: (path: string) => path,
+  cacheControl: {
+    browserTTL: 720,
+    edgeTTL: 720,
+    bypassCache: false,
+  },
+}
 /**
  * Streams files out of KV if it exists
- * @param options
+ * TODO KVNamespace should come from the wrangler config .. ?
  */
-export function kvStatic(options: KVStaticOptions): Middleware {
+export function kvStatic(kv: KVNamespace, mOptions: Partial<SiteInit>): Middleware {
   return async (ctx, next) => {
     // TODO: Store mime types in KV as well
     const contentTypes = {
@@ -30,18 +48,39 @@ export function kvStatic(options: KVStaticOptions): Middleware {
     if (!supportedExtensions.some(ext => ctx.request.url.endsWith('.' + ext))) {
       return next()
     }
+    // set up options from mOptions passed in and the default options
+    const options = Object.assign({}, defaultSiteInit, mOptions)
+    // set cache options by either evaluating the handler passed in
+    // or whatever settings were passed in
+    let evalCacheOpts: CacheOptions = (function() {
+      switch (typeof mOptions.cacheControl) {
+        case 'function':
+          return mOptions.cacheControl(ctx.request)
+        case 'object':
+          return mOptions.cacheControl
+        case 'undefined': //just returns default cache settings, but type safe
+          return typeof defaultSiteInit.cacheControl === 'function'
+            ? defaultSiteInit.cacheControl(ctx.request)
+            : defaultSiteInit.cacheControl
+      }
+    })()
+    const cacheOpts: CacheOptions = Object.assign({}, defaultSiteInit.cacheControl, evalCacheOpts)
 
+    // TODO first try to match request will cache
+    // TODO eval path handler
     const filename = ctx.request.url.substring(ctx.request.url.lastIndexOf('/') + 1)
     const ext = filename.substring(filename.lastIndexOf('.') + 1)
     const contentType = contentTypes[ext as keyof typeof contentTypes] || 'text'
-    const body = await options.kv.get(filename, 'stream')
+    const body = await kv.get(filename, 'stream')
 
-    if (!body) return ctx.end('', { status: 404 })
+    if (cacheOpts.edgeTTL && !cacheOpts.bypassCache) cache.put(filename, cacheOpts.edgeTTL)
+
+    if (!body) return ctx.end(await options.notFoundHandler(ctx.request))
 
     return ctx.end(body, {
       headers: {
         'Content-Type': contentType,
-        'Cache-Control': options.maxAge ? `max-age=${options.maxAge}` : 'public',
+        'Cache-Control': `max-age=${cacheOpts.browserTTL}`,
       },
     })
   }
